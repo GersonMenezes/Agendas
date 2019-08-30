e7fa68aef12bc5c1f977cb49bdfd774ba6ea99ea
gerson <gerson.getsemani@gmail.com>
2019-08-30 15:21:15 -0300

    Vou enviar

---
+++ Atividade 1.c
@@ -0,0 +1,29 @@
+// Exercício Ponteiros e Alocação de Memomia
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char* funcao(int n){
+    char *lista = (char*) malloc((n*10) * sizeof(char));
+    char *retorno = lista;
+    for(int x=0; x<n; x++){
+        printf("Diga o nome %d: ", x+1);
+        scanf("%s", lista);
+        lista += 10;
+    }
+    return retorno;
+}
+int main(){
+
+    int n;
+    char *p;
+    printf("Diga o número de elementos: ");
+    scanf("%d", &n);
+    p = funcao(n);
+    for(int x=0; x<n; x++){
+    printf("The list is: \n %s \n", p);
+    p+=10;
+    }
+return 0;
+}
